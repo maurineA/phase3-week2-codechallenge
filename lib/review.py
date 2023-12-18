58989359ea12071e140655d9b845fb61a30421d4
maurineA <maurinawambuia@gmail.com>
2023-12-18 12:47:17 +0300

    review initialization

--- lib/review.py
+++ lib/review.py
@@ -1,2 +1,9 @@
 class Review:
     reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+
+
