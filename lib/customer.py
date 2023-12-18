1d5664894ba79e5e84d1ba0adcae43fa2aca4005
maurineA <maurinawambuia@gmail.com>
2023-12-18 13:18:31 +0300

    appending the customer

--- lib/customer.py
+++ lib/customer.py
@@ -1,9 +1,10 @@
 class Customer:
-    Customers = []
+    customers = []
 
     def __init__(self, given_name, family_name):
         self.given_name = given_name
         self.family_name = family_name
+        Customer.customers.append(self)
       
     def given_name(self):
         return self.given_name
@@ -13,6 +14,11 @@ class Customer:
     
     def full_name(self):
         return f"{self.given_name} {self.family_name}"
+    
+    @classmethod
+    def all(clses):
+        return clses.all
+
 
 customer1 = Customer("John", "Doe")
 customer2 = Customer("Jane", "Smith")
