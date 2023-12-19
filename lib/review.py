c18b28d778fe9632f488f3cafd62b665cf826065
maurineA <maurinawambuia@gmail.com>
2023-12-19 11:20:20 +0300

    classes updates

--- lib/review.py
+++ lib/review.py
@@ -24,17 +24,17 @@ class Review:
     def restaurant(self):
         return self.restaurant
     
-# review1 = Review(customer1, restaurant1, 4)
-# review2 = Review(customer2, restaurant2, 5)
 
-# print(restaurant1.reviews())
-# print(restaurant2.customers())
+
+
     
 customer1 = Customer("John", "Doe")
 restaurant1 = Restaurant("Cheka")
 
 review1 = Review(customer1, restaurant1, 4)
 print(review1.get_rating())
+print(restaurant1.reviews())
+
     
     
 
